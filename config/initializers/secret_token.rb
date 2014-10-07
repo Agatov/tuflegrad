75ec411469803802fbdcf827d962823b1643d03a
agatov <agatovs@gmail.com>
2014-10-07 15:04:32 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuflegrad::Application.config.secret_key_base = 'b4ad82d6022edf201624230f446d534156d17b3fcd38daaa671414e95fbfc5a0479a5beea88b55436c1cb9eddd01e1b5d1fb81de3badf6337678c8b97432ffaf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tuflegrad::Application.config.session_store :cookie_store, key: '_tuflegrad_session'
